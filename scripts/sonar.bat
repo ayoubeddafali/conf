965f6b83ebb6b6af83a217625bca6e6fec0fd716
ayoubensalem <ayoubensalem@gmail.com>
2019-09-23 22:34:30 +0100

    add scripts

--- scripts/sonar.bat
+++ scripts/sonar.bat
@@ -1,12 +1,13 @@
 set pathMSBuild="C:\Program Files (x86)\MSBuild\14.0\Bin\msbuild.exe"
 set pathSonarQubeExe="C:\jenkins\sonar-scanner-msbuild\SonarScanner.MSBuild.exe"
+set nodeJsExe="C:\Program Files\nodejs\node.exe"
 set solutionName=%1
 set projectKey=%2
 set projectName=%3
 set projectVersion=%4
 set branchName=%5
 
-%pathSonarQubeExe% begin /k:"%projectKey%" /n:"%projectName%" /v:"%projectVersion%" /d:sonar.nodejs.executable="C:\Program Files\nodejs\node.exe" /d:sonar.css.node="C:\Program Files\nodejs\node.exe" /d:sonar.host.url="http://67.207.94.110:9000" /d:sonar.login="admin" /d:sonar.password="admin" 
+%pathSonarQubeExe% begin /k:"%projectKey%" /n:"%projectName%" /v:"%projectVersion%" /d:sonar.nodejs.executable="%nodeJsExe%" /d:sonar.host.url="http://67.207.94.110:9000" /d:sonar.login="admin" /d:sonar.password="admin" 
 %pathMSBuild% %solutionName% /t:rebuild /v:q /nologo
 %pathSonarQubeExe% end /d:sonar.login="admin" /d:sonar.password="admin"
 
